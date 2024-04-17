6b57e70de9634cda5546464aaaa3dd5ecf44697e
Mauri Junior <mvrjunior26@gmail.com>
2024-04-17 17:39:31 -0300

    Order, Instant, ISO 8601 / p2

--- src/main/java/com/woka/course/entities/Order.java
+++ src/main/java/com/woka/course/entities/Order.java
@@ -3,6 +3,10 @@ package com.woka.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,18 +17,20 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
-	@JoinColumn(name = "client_id") 
+	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
 	}
 
@@ -83,6 +89,5 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-	
 
 }
---
+++ src/main/java/com/woka/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.woka.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.woka.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+		
+	}
+
