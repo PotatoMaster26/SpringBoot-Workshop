cbf0083c33a14f22bb603df8699e9eaac03f7940
Mauri Junior <mvrjunior26@gmail.com>
2024-04-18 15:26:57 -0300

    Product

---
+++ src/main/java/com/woka/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.woka.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.woka.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+		
+	}
+
