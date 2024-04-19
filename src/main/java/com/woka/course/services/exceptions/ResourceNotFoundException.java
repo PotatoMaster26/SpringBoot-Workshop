3c582ab5457fc9a6bd4def36e38e7562862c1365
Mauri Junior <mvrjunior26@gmail.com>
2024-04-19 17:31:48 -0300

    Exception handling - findById

---
+++ src/main/java/com/woka/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.woka.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource no found. id " + id);
+	}
+
+}
